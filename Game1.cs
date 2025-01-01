705d9f4f9bcd9ac3e60706b32f9421cdbdd1c49f
Evan Hedgecock <evanjameshedgecock@gmail.com>
2024-12-31 23:51:09 -0700

    create earth and shuttle sprites in space with ticking score

--- Game1.cs
+++ Game1.cs
@@ -8,6 +8,13 @@ public class Game1 : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
+    
+    private Texture2D background;
+    private Texture2D shuttle;
+    private Texture2D earth;
+
+    private SpriteFont font;
+    private int score = 0;
 
     public Game1()
     {
@@ -27,6 +34,12 @@ public class Game1 : Game
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
+        background = Content.Load<Texture2D>("stars");
+        shuttle = Content.Load<Texture2D>("shuttle");
+        earth = Content.Load<Texture2D>("earth");
+
+        font = Content.Load<SpriteFont>("Score");
+
         // TODO: use this.Content to load your game content here
     }
 
@@ -35,7 +48,7 @@ public class Game1 : Game
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
 
-        // TODO: Add your update logic here
+        score++;
 
         base.Update(gameTime);
     }
@@ -44,7 +57,13 @@ public class Game1 : Game
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
-        // TODO: Add your drawing code here
+
+        _spriteBatch.Begin();
+        _spriteBatch.Draw(background, new Rectangle(0, 0, 800, 480), Color.CornflowerBlue);
+        _spriteBatch.Draw(earth, new Vector2(400, 240), Color.White);
+        _spriteBatch.Draw(shuttle, new Vector2(450, 240), Color.White);
+        _spriteBatch.DrawString(font, "Score: " + score, new Vector2(300, 50), Color.White);
+        _spriteBatch.End();
 
         base.Draw(gameTime);
     }
